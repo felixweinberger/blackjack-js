70ed3a66fa6348ec0f63b8b1731324189b61ebbc
Felix Weinberger <felix.weinberger@gmail.com>
2018-08-25 08:42:11 +0100

    provisional design finalized, priority is to implement logic now

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,38 @@
 $(document).ready(function(){
+
+  let hitButton = $("#hit-button");
+  let standButton = $("#stand-button");
+
+  hitButton.click(function() {
+    // to do
+  });
+
+  standButton.click(function() {
+    // to do
+  })
+
 });
 
+class GameState {
+  contstructor() {
+    this.deck;
+    this.dealerHand;
+    this.playerHand;
+    this.playerWins;
+    this.playerGames;
+  }
+}
+
+class Card {
+  constructor() {
+    this.suit;
+    this.value;
+    this.image;
+  }
+}
+
+function calculateHandScore(hand) {
+  // calculates the score of the hand, including aces etc
+}
+
+function 
